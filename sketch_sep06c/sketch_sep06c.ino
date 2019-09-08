93831a5a88d13c5babeac293ac9b485332490cb7
Nityananda Zbil <nityananda.zbil@icloud.com>
2019-09-08 06:26:00 +0200

    add rotation

--- sketch_sep06c/sketch_sep06c.ino
+++ sketch_sep06c/sketch_sep06c.ino
@@ -1,22 +1,44 @@
 #include <Servo.h>
+#include <IFX9201_XMC1300_StepperMotor.h>
 
+/*
+ * Alpha Wheel
+ */
+#define DIR_PIN 4    // Pin 9 is standard DIR Pin
+#define STP_PIN 3   // Pin 10 is standard STP Pin
+#define DIS_PIN 2   // Pin 11 is standard DIS Pin
+
+const int StepsPerRevolution = 200 * 8; // change this to fit the total number of steps per revolution for your motor
+
+Stepper_motor MyStepperMotor = Stepper_motor(StepsPerRevolution, DIR_PIN, STP_PIN, DIS_PIN);
+
+/*
+ * Spray Servo
+ */
+Servo BanksyServo;
+Servo BotServo;
+
+/*
+ * Commands
+ */
 // Nothing
-const int DONOTHING = 0;
-// Banksy
-const int SPRAY = 1;
-const int SHIFTLEFT = 2;
-const int SHIFTRIGHT = 3;
-// Bot
-const int TURNLEFT = 4;
-const int TURNRIGHT = 5;
+#define DONOTHING 0
+
+#define SPRAY 1
+
+#define ROTATECLOCKWISE 2
+#define ROTATEANTICLOCKWISE 3
+
+#define TURNLEFT 4
+#define TURNRIGHT 5
 
 const int STEPSFORSHIFT = 2000;
 
+/*
+ * Input
+ */
 String inputString = "";
 int InputCommand = 0;
-
-Servo BanksyServo;
-Servo BotServo;
 
 void setup()
 {
@@ -29,10 +51,19 @@ void setup()
   Serial.begin(9600);
   
   BanksyServo.attach(A0); // verknüpft den Servomotor an Pin 0 mit dem Servo-Objekt
-  BanksyServo.write(0); // überträgt die Zielposition an den Servomotors
+  BanksyServo.write(60); // überträgt die Zielposition an den Servomotors
 
   BotServo.attach(A1);
   BotServo.write(50);
+  
+  // set pins' mode as OUTPUT, set default speed and enable the stepper motor
+  MyStepperMotor.begin();
+
+  // set the speed at 10 rpm:
+  MyStepperMotor.setSpeed(40);
+
+  // initialize the serial port:
+  Serial.begin(9600);
 }
 
 void loop()
@@ -45,26 +76,14 @@ void executeCommand()
 {
   switch(InputCommand)
   {
-    case DONOTHING:
-      // Serial.println("Did nothing");
-      break;
-    case SPRAY:
-      spray(); 
-      break;
-    case SHIFTLEFT:
-      shiftLeft();
-      break;
-    case SHIFTRIGHT:
-      shiftRight();
-      break;
-    case TURNLEFT:
-      turn();
-      break;
-    case TURNRIGHT:
-      turn();
-      break;
-    default: 
-      break;
+    case DONOTHING: break;
+    case SPRAY: spray(); break;
+    case ROTATECLOCKWISE: rotateClockwise(); break;
+    case ROTATEANTICLOCKWISE: rotateAnticlockwise(); break;
+    
+    case TURNLEFT: turn(); break;
+    case TURNRIGHT: turn(); break;
+    default: break;
   }
 
   InputCommand = DONOTHING;
@@ -73,12 +92,12 @@ void executeCommand()
 void spray()
 {
   int pos = 0;
-  for (pos = 0; pos <= 90; pos += 2) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
+  for (pos = 60; pos >= 0; pos -= 2) { // goes from 180 degrees to 0 degrees
     BanksyServo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
-  for (pos = 90; pos >= 0; pos -= 2) { // goes from 180 degrees to 0 degrees
+  for (pos = 0; pos <= 60; pos += 2) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
     BanksyServo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
@@ -91,8 +110,20 @@ void spray()
   */
 }
 
-void shiftLeft()
+void rotateClockwise()
 {
+  Serial.println("moves 180 degree in clockwise direction");
+  MyStepperMotor.move_degree(360);
+  //MyStepperMotor.step(100);
+  delay(1000);
+
+  Serial.println("moves 180 degree in clockwise direction");
+  MyStepperMotor.move_degree(-360);
+  //MyStepperMotor.step(-100);
+  //delay(1000);
+
+  //digitalWrite(DIS_PIN, LOW);
+  /*
   if(digitalRead(2) == LOW)
   {
     digitalWrite(2, HIGH);
@@ -105,21 +136,14 @@ void shiftLeft()
     digitalWrite(4, LOW);
     delayMicroseconds(500);
   }
+  */
 }
 
-void shiftRight()
+void rotateAnticlockwise()
 {
-  if(digitalRead(2) == HIGH)
-  {
-    digitalWrite(2, LOW);
-  }
-  for(int i = 0; i < STEPSFORSHIFT; i++)
-  {
-    digitalWrite(4, HIGH);
-    delayMicroseconds(500);
-    digitalWrite(4, LOW);
-    delayMicroseconds(500);
-  }
+  MyStepperMotor.move_degree(-360);
+  delay(1000);
+  MyStepperMotor.move_degree(360);
 }
 
 void turn()
